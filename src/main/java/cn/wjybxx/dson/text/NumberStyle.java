a45a40166ff370877e6807485cff36a61b8a0ef3
houlei <845740757@qq.com>
2023-07-25 14:41:06 +0800

    long值打印时增加校验

--- src/main/java/cn/wjybxx/dson/text/NumberStyle.java
+++ src/main/java/cn/wjybxx/dson/text/NumberStyle.java
@@ -11,7 +11,6 @@ public enum NumberStyle implements INumberStyle {
 
     /**
      * 简单格式，能省略类型标签的情况下就省略标签
-     * 对于long类型的值要小心使用该模式，因为数字的默认解析类型是double，如果值超过了double的表示范围，将无法正确解析。
      */
     SIMPLE(1) {
         @Override
@@ -22,6 +21,10 @@ public enum NumberStyle implements INumberStyle {
         @Override
         public void toString(long value, StyleOut styleOut) {
             styleOut.setValue(Long.toString(value));
+            // 数字的默认解析类型是double，如果值超过了double的表示范围，将无法正确解析
+            if (Math.abs(value) >= DOUBLE_MAX_LONG) {
+                styleOut.setTyped(true);
+            }
         }
 
         @Override
@@ -70,6 +73,10 @@ public enum NumberStyle implements INumberStyle {
         @Override
         public void toString(long value, StyleOut styleOut) {
             styleOut.setValue(Long.toString(value));
+            // 数字的默认解析类型是double，如果值超过了double的表示范围，将无法正确解析
+            if (Math.abs(value) >= DOUBLE_MAX_LONG) {
+                styleOut.setTyped(true);
+            }
         }
 
         @Override
@@ -363,6 +370,9 @@ public enum NumberStyle implements INumberStyle {
             default -> throw new IllegalArgumentException("invalid number " + number);
         };
     }
+
+    /** double能精确表示的最大整数 */
+    private static final long DOUBLE_MAX_LONG = (1L << 53) - 1;
 
     private static String toStringNoSci(float value) {
         return new BigDecimal(Float.toString(value)) // 需先转换为String
